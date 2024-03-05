fdbe9071217841d2f16d4d8d14ca65a74e89114b
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-03-06 07:31:16 +0900

    課題3の提出

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[new create]
+  resources :sessions, only: [:new, :create, :destroy]
 
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
   root "static_pages#top"
