83c52a36dcafe0eb8ac525dc6227c5e2b906ebe3
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-03-17 18:11:31 +0900

    課題7の提出3

---
+++ db/migrate/20240317090129_add_boards.rb
@@ -0,0 +1,4 @@
+class AddBoards < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_01_234025) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_17_090224) do
+  create_table "boards", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", null: false
     t.string "crypted_password"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_01_234025) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "boards", "users"
 end
