ebbeb4c2bebf835f53e89df696d24832069f825e
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-03-25 06:47:16 +0900

    課題10の提出2

--- app/uploaders/board_image_uploader.rb
+++ app/uploaders/board_image_uploader.rb
@@ -39,7 +39,7 @@ class BoardImageUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
   def extension_allowlist
-    %w(jpg jpeg gif png)
+    %w('jpg' 'jpeg' 'gif' 'png')
   end
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
