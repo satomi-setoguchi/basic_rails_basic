8b48bc0d211a8419759b47b20251d9b3a7496f3a
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-05-05 16:25:34 +0900

    課題16の提出2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,19 +8,6 @@ class CommentsController < ApplicationController
     @comment.save
   end
 
-  def edit
-    comment = Comments.find(params[:id])
-  end
-
-  def update
-    comment = current_user.comments.find(params[:id])
-    if comment.update(comment_params)
-      redirect_to board_path(comment), success: t('comment.create.success')
-    else
-      redirect_to board_path(comment), danger: t('comment.create.failure')
-    end
-  end
-
   def destroy
     @comment = current_user.comments.find(params[:id])
     @comment.destroy!
