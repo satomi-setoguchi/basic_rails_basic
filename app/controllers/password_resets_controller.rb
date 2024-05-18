4cad05ef5e63850e5cf4f16c48621fd44b3297b2
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-05-18 09:43:40 +0900

    課題20の提出2

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,38 +1,35 @@
 class PasswordResetsController < ApplicationController
-    skip_before_action :require_login
+  skip_before_action :require_login
 
-    def new; end
-  
-    def create
-      @user = User.find_by(email: params[:email])
-      @user&.deliver_reset_password_instructions!
-      redirect_to login_path, success: 'パスワードリセット手順を送信しました'
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path, success: 'パスワードリセット手順を送信しました'
+  end
+
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+    not_authenticated if @user.blank?
+  end
+
+  def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
     end
   
-    def edit
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(params[:id])
-      if @user.blank?
-        not_authenticated
-        return
-      end
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password(params[:user][:password])
+      redirect_to login_path, success: 'パスワードを変更しました'
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: 'パスワード')
+      render :edit, status: :unprocessable_entity
     end
-  
-    def update
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(params[:id])
-  
-      if @user.blank?
-        not_authenticated
-        return
-      end
-  
-      @user.password_confirmation = params[:user][:password_confirmation]
-      if @user.change_password(params[:user][:password])
-        redirect_to login_path, success: 'パスワードを変更しました'
-      else
-        flash.now[:danger] = t('defaults.flash_message.not_updated', item: 'パスワード')
-        render :edit, status: :unprocessable_entity
-      end
-    end
+  end
 end
--- db/migrate/20240512012352_sorcery_reset_password.rb
+++ db/migrate/20240512012352_sorcery_reset_password.rb
@@ -1,6 +1,6 @@
 class SorceryResetPassword < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :reset_password_token, :string, default: nil
+    add_column :users, :reset_password_token, :string, default: nil, index: { unique: true }
     add_column :users, :reset_password_token_expires_at, :datetime, default: nil
     add_column :users, :reset_password_email_sent_at, :datetime, default: nil
     add_column :users, :access_count_to_reset_password_page, :integer, default: 0
