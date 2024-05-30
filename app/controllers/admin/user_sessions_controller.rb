5140fea0c328bed2746df0ae63c73c08e7b34474
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-05-30 13:41:56 +0900

    課題21の提出3

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class Admin::UserSessionsController < Admin::BaseController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to admin_root_path ,success: 'ログインしました'
+      redirect_to admin_root_path, success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
