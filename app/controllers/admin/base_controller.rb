39708583d5f0749cede6beb000dddf4b49829cff
瀬戸口理美 <setoguchisatomi@setoguchisatominoiMac.local>
2024-05-30 13:36:34 +0900

    課題21の提出2

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,15 +1,15 @@
 class Admin::BaseController < ApplicationController
   before_action :check_admin
   layout 'admin/layouts/application'
- 
+
   private
- 
+
   def not_authenticated
     flash[:warning] = t('defaults.flash_message.require_login')
     redirect_to admin_login_path
   end
- 
+
   def check_admin
-    redirect_to root_path, danger: "管理者権限がありません" unless current_user.admin?
+    redirect_to root_path, danger: '管理者権限がありません' unless current_user.admin?
   end
 end
--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UserSessionsController < Admin::BaseController
   skip_before_action :require_login, only: %i[new create]
   skip_before_action :check_admin, only: %i[new create]
-   
+
   layout 'layouts/admin_login'
 
   def new; end
@@ -9,7 +9,7 @@ class Admin::UserSessionsController < Admin::BaseController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to admin_root_path  , success: 'ログインしました'
+      redirect_to admin_root_path ,success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
